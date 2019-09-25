1f35e50346536f5b226c7e08b219975fd3d4c416
Jake <jakefantin@gmail.com>
2019-09-25 20:49:19 +0000

    refactored input control

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-const handleUserInput = function (data){
-  if(data === '\u0003'){
-    process.exit();
-  }
-}
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (data) => {handleUserInput(data)});
-  return stdin;
-
-}
-
-stdin = setupInput();
 console.log('Connecting ...');
 connect();
+
+setupInput();
